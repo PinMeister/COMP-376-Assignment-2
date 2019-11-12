d43c946514f9f026f24b3f74e7e2e688358a468b
Patrick Pin <patrickpin840@hotmail.com>
2019-11-12 16:40:26 -0500

    Add assets and project settings

---
+++ Assets/Scripts/Piranha.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+
+public class Piranha : MonoBehaviour
+{
+    [SerializeField]
+    float positionOffset = 1;
+    [SerializeField]
+    public int boundaryMax = 2;
+    [SerializeField]
+    public float moveSpeed = 1;
+
+    int boundaryCounter;
+    Transform leftBoundary;
+    Transform rightBoundary;
+    GameSpawner gameSpawnerVariable;
+
+    void Start()
+    {
+        boundaryCounter = 0;
+        leftBoundary = GameObject.Find("LeftBoundary").transform;
+        rightBoundary = GameObject.Find("RightBoundary").transform;
+        gameSpawnerVariable = GameObject.Find("GameSpawner").GetComponent<GameSpawner>();
+    }
+
+    void Update()
+    {
+        if (GameObject.Find("Player").GetComponent<Player>().alive)
+        {
+            if (GetComponent<SpriteRenderer>().flipX == true)
+            {
+                transform.Translate(Vector3.left * moveSpeed * Time.deltaTime);
+            }
+            else
+            {
+                transform.Translate(Vector3.right * moveSpeed * Time.deltaTime);
+
+            }
+        }
+
+        if (transform.position.x > rightBoundary.position.x - positionOffset - 1)
+        {
+            transform.position = new Vector2(leftBoundary.position.x + positionOffset, transform.position.y);
+            boundaryCounter += 1;
+        }
+        if (transform.position.x < leftBoundary.position.x + positionOffset)
+        {
+            transform.position = new Vector2(rightBoundary.position.x - positionOffset - 1, transform.position.y);
+            boundaryCounter += 1;
+        }
+
+        if (boundaryCounter == boundaryMax)
+        {
+            gameSpawnerVariable.piranhaYList.Add((int)(transform.position.y));
+            gameSpawnerVariable.piranhaCounter -= 1;
+            Destroy(gameObject);
+        }
+
+    }
+}
